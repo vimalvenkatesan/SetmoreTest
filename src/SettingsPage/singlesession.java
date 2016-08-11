0e04d28271310d1df7b1d75bf72fbae132d9db01
vimalvenkatesan <vimalvenkatesanvv@gmail.com>
2016-08-11 14:02:52 +0530

    Ready with single session

--- src/SettingsPage/singlesession.java
+++ src/SettingsPage/singlesession.java
@@ -1,11 +1,9 @@
 package SettingsPage;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,17 +14,16 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import Utility.Bookingpage;
 import Utility.Constant;
 import Utility.Reporter;
-import customerPage.AddCustomer;
 @Listeners(Reporter.class)
 public class singlesession {
+	//WebDriverWait(driver,5000);
 	
-	WebDriverWait wait=new WebDriverWait(driver, 60,5000); 
+	    WebDriverWait wait=new  WebDriverWait(driver,30);
 
 	
-	   public static WebDriver driver=new FirefoxDriver();
+	    public static WebDriver driver=new FirefoxDriver();
 	    private static Logger Log = Logger.getLogger(singlesession.class.getName());
 	
 	    @BeforeClass
@@ -49,8 +46,7 @@ public class singlesession {
 	    public void SettingsTab()
 	    {	
 		 
-	     Log.info("Settings Tab Clicked ");
-	     
+	     Log.info("Settings Tab Clicked ");     
 	     driver.findElement(By.xpath(".//*[@id='settings_new']/a")).click();
 	     WebElement ele = driver.findElement(By.xpath(".//*[@id='settings_new']/ul/li[4]/a/div[2]"));
 	     Actions Builder=new Actions(driver);
@@ -58,23 +54,23 @@ public class singlesession {
 	    
 	
 	    try {
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	    }
-	    @SuppressWarnings("unused")
+	    
+	    
+	    
 		@Test(priority=2)
 	    public void createservices() throws InterruptedException
 	    { 
-	    Log.info("Create class");
-	    
-	    
-	    //Boolean isPresent = driver.findElements(By.xpath(".//*[@id='noClass']/button")).size() > 0;
-	    
-	   // System.out.println(isPresent);
-	    
+	    	
+	    Log.info("Create class");  
+	    //Boolean isPresent = driver.findElements(By.xpath(".//*[@id='noClass']/button")).size() > 0;   
+	    // System.out.println(isPresent);
+    
 	    if(driver.findElements(By.xpath(".//*[@id='noClass']/button")).size() > 0){
 	  
 	    	driver.findElement(By.xpath(".//*[@id='noClass']/button")).click();
@@ -85,6 +81,7 @@ public class singlesession {
 	  
 	    
 	    driver.findElement(By.xpath(".//*[@id='class_cost']")).click();
+	    driver.findElement(By.xpath(".//*[@id='class_cost']")).clear();
 	    driver.findElement(By.xpath(".//*[@id='class_cost']")).sendKeys("100");
 	  
 	    
@@ -100,31 +97,29 @@ public class singlesession {
 	    
 	    
 	    }else
+	    	
 	    {
+	    	
 	    //Click class id 
-	    driver.findElement(By.xpath(".//*[@id='cafd651470643859287']/div[3]")).click(); 
+	    driver.findElement(By.xpath(".//*[@id='caa90c1470897188384']/div[3]")).click(); 
 	    
-	    for(int j=0; j<=1; j++)
-	    
+	    for(int j = 1; j <= 3; j++)
 	    {
-	  
-	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='addClassSession']")));
-	    
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='addClassSession']")));    
 	    driver.findElement(By.xpath(".//*[@id='addClassSession']")).click();
 	    Thread.sleep(10000);
+	    
+	    //Update seats
 	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[6]/input")));
-	    
-	    //click session pop-up   
-       // driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]")).click();
-	    
-	   //Update seats
 	    driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[6]/input")).click();  
 	    Thread.sleep(5000);
-	    driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[6]/input")).sendKeys("1000");
+	    driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[6]/input")).clear();
+	    Thread.sleep(5000);
+	    driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[6]/input")).sendKeys("10000");
 	    Thread.sleep(2000);
 	  
 	    //Click Never select Box
-	   // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	    // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	    driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[5]/div[1]")).click();
 	    driver.findElement(By.xpath(".//*[@id='editClassApptPopup']/div[2]/ul/li[5]/div[1]/ul/li/div[1]")).click();
 	   
@@ -162,18 +157,19 @@ public class singlesession {
 	      WebElement nextArrow	=   lastPicker.findElement(By.xpath(".//div[@class='datepickerContainer']/table/tbody/tr/td/table/thead/tr[1]/th[3]/a"));
 	    
 	      
-	      for(int i = 1; i <= 10;i++)
+	      for(int i = 1; i <= 30;i++)
 	      {
 	    	  nextArrow.click();
-	      }
+	      }														 
+	  //    WebElement date30May = lastPicker.findElement(By.xpath(".//div[@class='datepickerContainer']/table/tbody/tr/td/table/tbody[2]/tr[4]/td[2]"));
 	      
-	      WebElement date30july = lastPicker.findElement(By.xpath(".//div[@class='datepickerContainer']/table/tbody/tr/td/table/tbody[2]/tr[5]/td[5]/a"));
-	      date30july.click();
+	      WebElement July = lastPicker.findElement(By.xpath(".//div[@class='datepickerContainer']/table/tbody/tr/td/table/tbody[2]/tr[5]/td[3]/a"));
+	      July.click();
 	      
 	    //Save;
 	    driver.findElement(By.className("class-popup-details-save-btn")).click();
 	    System.out.println("Booking in process");
-	    Thread.sleep(80000);
+	    Thread.sleep(120000);
 	    System.out.println("Next Iterations");
 	  
 	    }
@@ -181,4 +177,4 @@ public class singlesession {
 	    }
 	   
 	    }
-	    }
+}
